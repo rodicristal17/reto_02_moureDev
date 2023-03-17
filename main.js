c76907e2be6662dece015253ad9dce6f11c1d3e4
rodicristal17 <rodicristal17@gmail.com>
2023-03-17 13:46:53 -0300

    actualización para dos palabras

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-function isAnagrama(word) {
-    for (let i = 0; i < word.length; i++) {
-        if (word[i] !== word[word.length - 1 - i]) {
+function isAnagrama(word1, word2) {
+    for (let i = 0; i < word1.length; i++) {
+        if (word1[i] !== word1[word1.length - 1 - i]) {
+            return false;
+        }
+    }
+    for (let j = 0; j < word2.length; j++) {
+        if (word2[j] !== word2[word2.length - 1 - j]) {
             return false;
         }
     }
@@ -8,4 +13,4 @@ function isAnagrama(word) {
     return true;
 }
 
-console.log(isAnagrama("ana"));
+console.log(isAnagrama("ana", "ana"));

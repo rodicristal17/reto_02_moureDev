d2d488fe5ac4b9208e6abe6f34933da29a82815d
rodicristal17 <rodicristal17@gmail.com>
2023-03-17 13:10:42 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+function isAnagrama(word) {
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] !== word[word.length - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(isAnagrama("ana"));
